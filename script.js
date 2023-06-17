734eeacdf51b6d1180d5e95c43475417296af255
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-17 20:47:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your code here
+function findMajorityElement(nums) {
+  let count = 0;
+  let candidate;
+
+  // Find potential majority element
+  for (let i = 0; i < nums.length; i++) {
+    if (count === 0) {
+      candidate = nums[i];
+    }
+
+    count += candidate === nums[i] ? 1 : -1;
+  }
+
+  // Verify if the candidate is the majority element
+  count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === candidate) {
+      count++;
+    }
+  }
+
+  // Return the majority element
+  if (count > Math.floor(nums.length / 2)) {
+    return candidate;
+  }
+
+  // If no majority element is found (this should not occur)
+  return null;
+}
+
+// Example usage
+const nums = [2, 1, 2];
+const majorityElement = findMajorityElement(nums);
+console.log("Majority Element:", majorityElement);
